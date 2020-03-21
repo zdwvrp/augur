20e9f4e48e85ac09a3b2b049086604dafc61a5a4
michaelwoodruffdev <michaelwoodruffdev@gmail.com>
2020-03-21 14:13:53 -0500

    add SlackConfig page content for when user is not logged in

--- augurface/src/layouts/SlackConfig/SlackConfig.vue
+++ augurface/src/layouts/SlackConfig/SlackConfig.vue
@@ -1,15 +1,44 @@
 <template>
   <div id="SlackConfig">
-      
+    <div id="not-logged-in" v-if="!isLoggedIn">
+      <img src="../../assets/slackbutton.png" alt="sign in with slack" @click="signInWithSlack" />
+      <p>Log in via Slack to configure tracked repositories</p>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
-    name: 'SlackCongig'
-}
+  name: "SlackCongig", 
+  methods: {
+      ...mapActions("userModule", ["signInWithSlack"])
+  }, 
+  computed: {
+      ...mapGetters("userModule", ["isLoggedIn"])
+  }
+};
 </script>
 
 <style>
+    #not-logged-in {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        margin-top: 7rem;
+    }
 
+    #not-logged-in > img {
+        width: 20rem;
+        background-color: var(--grey);
+        border-radius: .5rem;
+        transition: box-shadow .3s ease;
+    }
+
+    #not-logged-in > img:hover {
+        cursor: pointer;
+        box-shadow: 0 0 4px 0 var(--grey);
+    }
 </style>
--- augurface/src/store/UserModule.js
+++ augurface/src/store/UserModule.js
@@ -1,11 +1,21 @@
 // module for necessary user data
+import Vue from "vue";
+
 export default {
     namespaced: true, 
     state: {
         loggedIn: false
     }, 
-    mutations: {}, 
-    actions: {}, 
+    mutations: {
+        setLoggedIn(state, newValue) {
+            Vue.set(state, 'loggedIn', newValue);
+        }
+    }, 
+    actions: {
+        signInWithSlack: (context) => {
+            context.commit("setLoggedIn", true);
+        }
+    }, 
     getters: {
         isLoggedIn(state) {
             return state.loggedIn;
