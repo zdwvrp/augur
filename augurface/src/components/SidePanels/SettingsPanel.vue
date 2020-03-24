d8dc379d97ae88ff933c8965c90bfc48e7021173
michaelwoodruffdev <michaelwoodruffdev@gmail.com>
2020-03-24 12:10:35 -0500

    implement dropdown in settings panel for switch augur hosts to make requests to

--- augurface/src/components/SidePanels/SettingsPanel.vue
+++ augurface/src/components/SidePanels/SettingsPanel.vue
@@ -1,15 +1,45 @@
 <template>
   <div id="SettingsPanel">
-      hello component within slot
+      <aug-dropdown class="dropdown" @selectionChanged="setBaseEndpointUrl" :options="getAvailableEndpointsForDropdown" :defaultOption="getCurrentEndpointUrlForDropdown">augur host</aug-dropdown>
   </div>
 </template>
 
 <script>
+import AugDropdown from "../BaseComponents/AugDropdown.vue";
+import { mapMutations, mapGetters } from "vuex";
+
 export default {
-    name: "SettingsPanel"
+    name: "SettingsPanel", 
+    components: {
+        AugDropdown
+    }, 
+    data() {
+        return {
+
+        }
+    }, 
+    methods: {
+        ...mapMutations("utilModule", ["setBaseEndpointUrl"])
+    }, 
+    computed: {
+        ...mapGetters("utilModule", ["getCurrentEndpointUrlForDropdown", "getAvailableEndpointsForDropdown"])
+    }
 }
 </script>
 
-<style>
+<style scoped>
+#SettingsPanel {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+}
 
+#SettingsPanel > * {
+    margin-top: 1rem;
+}
+
+.dropdown {
+    width: 80% !important;
+}
 </style>
